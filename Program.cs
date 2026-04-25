bd0671a2c056fcbb4cb6171e9c60dbe0d2e28fa7
Thobani613 <thobanimjiyakho7@gmail.com>
2026-04-25 07:44:05 +0200

    Initial console student manager app

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("=== STUDENT MANAGER APP ===");
+Console.WriteLine();
+
+//get student name
+Console.Write("Enter student name: "); 
+string? studentName = Console.ReadLine();
+
+//get student number which be stored as a string
+Console.WriteLine("Enter student number: ");
+string? studentNumber = Console.ReadLine();
+
+//get Ccourse
+Console.WriteLine("Enter course name: ");
+string? course = Console.ReadLine();
+
+//code to display the information entered by the user
+Console.WriteLine();
+Console.WriteLine("=== Student Details ===");
+Console.WriteLine($"Name: {studentName}");
+Console.WriteLine($"Student Number: {studentNumber}");
+Console.WriteLine($"Course: {course}");
