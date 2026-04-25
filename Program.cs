01e1d7a1216eee6eecabe38ff2852c070781b80c
Thobani613 <thobanimjiyakho7@gmail.com>
2026-04-25 08:45:07 +0200

    Add console menu for student manager

--- Program.cs
+++ Program.cs
@@ -1,21 +1,73 @@
-﻿Console.WriteLine("=== STUDENT MANAGER APP ===");
-Console.WriteLine();
+﻿string? studentName = null;
+string? studentNumber = null;
+string? course = null;
+bool isRunning = true;
 
-//get student name
-Console.Write("Enter student name: "); 
-string? studentName = Console.ReadLine();
+//while loop for when is running
+while (isRunning)
+{
+    Console.Clear();
+    Console.WriteLine("=== Student Manager ===");
+    Console.WriteLine("1. Add Student");
+    Console.WriteLine("2. View Student");
+    Console.WriteLine("3. Exit");
 
-//get student number which be stored as a string
-Console.WriteLine("Enter student number: ");
-string? studentNumber = Console.ReadLine();
+    //we ask the user to choose an option 
+    Console.WriteLine("Choose an option: ");
+    string? choice = Console.ReadLine();
+    
+    //switch statement for for the option selected
+    switch (choice)
+    {
+        case "1":
+        Console.Clear();
 
-//get Ccourse
-Console.WriteLine("Enter course name: ");
-string? course = Console.ReadLine();
+        Console.WriteLine("=== Add student === ");
+        Console.Write("Enter student name: ");
+        studentName = Console.ReadLine();
 
-//code to display the information entered by the user
-Console.WriteLine();
-Console.WriteLine("=== Student Details ===");
-Console.WriteLine($"Name: {studentName}");
-Console.WriteLine($"Student Number: {studentNumber}");
-Console.WriteLine($"Course: {course}");
+        Console.Write("Enter student number: ");
+        studentNumber = Console.ReadLine();
+
+        Console.Write("Enter course name: ");
+        course = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Student added successfully");
+        Console.WriteLine("Press any key to exit to the menu");
+        Console.ReadKey();
+
+        break;
+
+        case "2":
+        Console.Clear();
+        Console.WriteLine("=== Student details ===");
+
+        if (string.IsNullOrWhiteSpace(studentName))
+            {
+                Console.WriteLine("No student has been added yet");
+            }
+            else
+            {
+                Console.WriteLine($"Name: {studentName}");
+                Console.WriteLine($"Student Number: {studentNumber}");
+                Console.WriteLine($"Course: {course}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to retrn to the main menu");
+            Console.ReadKey();
+
+        break;
+
+        case "3":
+        isRunning = false;
+        break;
+
+        default:
+        Console.WriteLine("Invalid option. Please choose 1, 2, or 3.");
+        Console.WriteLine("Press any key to try again...");
+        Console.ReadKey();
+        break;
+    }
+}
